47d73e8d750e233de86741343899e907baeaa275
Kuba <czubakjakub94@gmail.com>
2023-01-11 14:00:57 +0100

    added drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Login } from "./components/login/Login";
+import { Infrabox } from "./components/infrabox/Infrabox";
+import { useState } from 'react'
 
 function App() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Infrabox />
     </div>
   );
 }
---
+++ src/components/infrabox/Infrabox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from './Header'
+import { NavSidebar } from './NavSidebar'
+
+export const Infrabox = () => {
+  return (
+    <div>
+        <NavSidebar />
+    </div>
+  )
+}
