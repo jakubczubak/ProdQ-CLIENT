7e6e2ed86c08f8bf52f494477b511f642657d8da
Kuba <czubakjakub94@gmail.com>
2023-01-11 19:06:20 +0100

    added favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Login } from "./components/login/Login";
 import { Infrabox } from "./components/infrabox/Infrabox";
-import { useState } from 'react'
+import { useEffect } from "react";
 
 function App() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  useEffect(() => {
+    document.title = "Infrabox";
+  }, []);
+
   return (
     <div className="App">
       {/* <Login /> */}
