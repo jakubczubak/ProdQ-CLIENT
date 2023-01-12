723199f3fa0ed565d6b643619e7fe0fcc7c039d8
Kuba <czubakjakub94@gmail.com>
2023-01-12 12:01:09 +0100

    added Header component

--- src/components/infrabox/Infrabox.jsx
+++ src/components/infrabox/Infrabox.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from './Header'
 import { NavSidebar } from './NavSidebar'
+import { Main } from './Main'
 
 export const Infrabox = () => {
   return (
     <div>
         <NavSidebar />
         <Header />
+        <Main />
     </div>
   )
 }
---
+++ src/components/infrabox/Main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Main.module.css'
+
+export const Main = () => {
+  return (
+    <div className={styles.main_container}>Main</div>
+  )
+}
