5ad32b2ef7bd98f5fb5840a1e0ede413c83942a6
Kuba <czubakjakub94@gmail.com>
2023-01-27 13:08:38 +0100

    refactor

--- src/components/material/Dimensions.jsx
+++ src/components/material/Dimensions.jsx
@@ -117,6 +117,44 @@ export const Dimensions = ({ control, type }) => {
           />
         </Stack>
       )}
+      {type == 'Rod' && (
+        <Stack spacing={1} mt={2} direction="row">
+          <Controller
+            name="diameter"
+            control={control}
+            render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+              <Input
+                error={error}
+                placeholder="⌀ 20"
+                onBlur={onBlur}
+                value={value}
+                onChange={onChange}
+                label="Diameter"
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">mm</InputAdornment>
+                }}
+              />
+            )}
+          />
+          <Controller
+            name="length"
+            control={control}
+            render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+              <Input
+                error={error}
+                placeholder="1000"
+                onBlur={onBlur}
+                value={value}
+                onChange={onChange}
+                label="Length"
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">mm</InputAdornment>
+                }}
+              />
+            )}
+          />
+        </Stack>
+      )}
     </>
   );
 };
