9b915fefa8b3e2048c0d095e3e1d27df2b565b53
Kuba <czubakjakub94@gmail.com>
2023-02-21 20:53:32 +0100

    refactor

--- src/components/tool/ToolModal_EDIT.jsx
+++ src/components/tool/ToolModal_EDIT.jsx
@@ -14,6 +14,7 @@ import { toolManager } from './service/toolManager';
 export const ToolModal_EDIT = ({ onClose, item, toolListItem, updateTable }) => {
   const { handleSubmit, control, reset } = useForm({
     defaultValues: {
+      id: toolListItem.id,
       dc: toolListItem.dc,
       cfl: toolListItem.cfl,
       oal: toolListItem.oal,
