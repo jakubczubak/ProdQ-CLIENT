d1e009f6ee01479453839dd553e43f7e0ebfc4f3
Kuba <czubakjakub94@gmail.com>
2023-03-20 12:07:39 +0100

    edited supplier section

--- src/components/supplier/SupplierForm.jsx
+++ src/components/supplier/SupplierForm.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { Breadcrumbs, Typography } from '@mui/material';
+import { Breadcrumbs, Typography, Stack, TextField, IconButton } from '@mui/material';
 import styles from './css/SupplierForm.module.css';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Input } from '../common/Input';
+import AddIcon from '@mui/icons-material/Add';
 
 export const SupplierForm = () => {
+  const { handleSubmit, control, reset } = useForm({
+    defaultValues: {
+      materialGroupName: '',
+      type: '',
+      image: '',
+      material: null
+    }
+    // resolver: yupResolver(materialGroupValidationSchema)
+  });
+
+  const handleForm = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
       <Breadcrumbs
@@ -31,7 +50,143 @@ export const SupplierForm = () => {
             <button className={styles.supplierFrom_info_button}>View Company Page</button>
           </div>
         </div>
-        <div className={styles.supplierFrom_details_container}></div>
+        <div className={styles.supplierFrom_details_container}>
+          <p className={styles.supplierFrom_details_title}>Supplier Details</p>
+          <form onSubmit={handleSubmit(handleForm)}>
+            <Stack spacing={2} mb={2} className={styles.login_content} direction="row">
+              <Controller
+                name="name"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <Input
+                    error={error}
+                    placeholder="Monika"
+                    onBlur={onBlur}
+                    value={value}
+                    onChange={onChange}
+                    label="Name"
+                  />
+                )}
+              />
+              <Controller
+                name="surname"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <Input
+                    error={error}
+                    placeholder="Orzechowska"
+                    onBlur={onBlur}
+                    value={value}
+                    onChange={onChange}
+                    label="Surname"
+                  />
+                )}
+              />
+            </Stack>
+            <Stack spacing={2} mb={2} className={styles.login_content} direction="row">
+              <Controller
+                name="phoneNumber"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <Input
+                    error={error}
+                    placeholder="+48 732 489 006"
+                    onBlur={onBlur}
+                    value={value}
+                    onChange={onChange}
+                    label="Phone Number"
+                  />
+                )}
+              />
+              <Controller
+                name="email"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <Input
+                    error={error}
+                    placeholder="monika.o@gmail.com"
+                    onBlur={onBlur}
+                    value={value}
+                    onChange={onChange}
+                    label="Email Address"
+                  />
+                )}
+              />
+            </Stack>
+            <Stack spacing={2} mb={2} className={styles.login_content} direction="row">
+              <Controller
+                name="compnayName"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <Input
+                    error={error}
+                    placeholder="Adamet"
+                    onBlur={onBlur}
+                    value={value}
+                    onChange={onChange}
+                    label="Company Name"
+                  />
+                )}
+              />
+              <Controller
+                name="CompanyAddress"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <Input
+                    error={error}
+                    placeholder="Warszawa, ul. Kolejowa 12"
+                    onBlur={onBlur}
+                    value={value}
+                    onChange={onChange}
+                    label="Company Address"
+                  />
+                )}
+              />
+            </Stack>
+            <Stack spacing={2} mb={2} className={styles.login_content} direction="row">
+              <Controller
+                name="compnayLogo"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <Input
+                    error={error}
+                    placeholder="URL"
+                    onBlur={onBlur}
+                    value={value}
+                    onChange={onChange}
+                    label="Company Logo"
+                    variant="filled"
+                  />
+                )}
+              />
+              <Controller
+                name="compnayLogo"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <Input
+                    error={error}
+                    placeholder="URL"
+                    onBlur={onBlur}
+                    value={value}
+                    onChange={onChange}
+                    label="Company Link"
+                    variant="filled"
+                  />
+                )}
+              />
+            </Stack>
+            <div className={styles.tag_wrapper}>
+              <TextField id="standard-basic" label="ADD TAG" variant="standard" />
+              <IconButton>
+                <AddIcon />
+              </IconButton>
+            </div>
+            <div className={styles.tag_list}>
+              <button className={styles.tag_item}>1 tag</button>
+            </div>
+            <button type="submit">CREATE SUPPLIER</button>
+          </form>
+        </div>
       </div>
     </>
   );
