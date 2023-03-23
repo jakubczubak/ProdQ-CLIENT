1ce87d93e1cd53f036db30e061035349d174fb27
Kuba <czubakjakub94@gmail.com>
2023-03-23 11:25:52 +0100

    added setting component

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import styles from './css/Settings.module.css';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { Box, Tab } from '@mui/material';
+import { useState } from 'react';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import { UserList } from './UserList';
+import { UserDetails } from './UserDetails';
 
 export const Settings = () => {
-  return <div>Settings</div>;
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <div className={styles.settings_container}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="Tabs example">
+            <Tab label="My profile" value="1" />
+            <Tab
+              label="Users list"
+              value="2"
+              icon={<AdminPanelSettingsIcon />}
+              iconPosition="end"
+            />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <UserDetails />
+        </TabPanel>
+        <TabPanel value="2">
+          <UserList />
+        </TabPanel>
+      </TabContext>
+    </div>
+  );
 };
