121b2964acc4847cc621af3cf72b70c4a45b3176
Kuba <czubakjakub94@gmail.com>
2023-03-23 13:04:28 +0100

    edited settings section

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './css/Settings.module.css';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { Box, Tab } from '@mui/material';
+import { Box, Tab, Breadcrumbs, Typography } from '@mui/material';
 import { useState } from 'react';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import { UserList } from './UserList';
@@ -14,26 +14,41 @@ export const Settings = () => {
     setValue(newValue);
   };
   return (
-    <div className={styles.settings_container}>
-      <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChange} aria-label="Tabs example">
-            <Tab label="My profile" value="1" />
-            <Tab
-              label="Users list"
-              value="2"
-              icon={<AdminPanelSettingsIcon />}
-              iconPosition="end"
-            />
-          </TabList>
-        </Box>
-        <TabPanel value="1">
-          <UserDetails />
-        </TabPanel>
-        <TabPanel value="2">
-          <UserList />
-        </TabPanel>
-      </TabContext>
-    </div>
+    <>
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        separator={<Typography color="text.primary">/</Typography>}>
+        <Typography color="text.primary">...</Typography>
+
+        <Typography color="text.primary">Settings</Typography>
+      </Breadcrumbs>
+      <div className={styles.header}>
+        <Typography variant="h5" component="div">
+          Settings
+        </Typography>
+      </div>
+
+      <div className={styles.settings_container}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label="Tabs example">
+              <Tab label="My profile" value="1" />
+              <Tab
+                label="Users list"
+                value="2"
+                icon={<AdminPanelSettingsIcon />}
+                iconPosition="end"
+              />
+            </TabList>
+          </Box>
+          <TabPanel value="1">
+            <UserDetails />
+          </TabPanel>
+          <TabPanel value="2">
+            <UserList />
+          </TabPanel>
+        </TabContext>
+      </div>
+    </>
   );
 };
--- src/components/settings/UserDetails.jsx
+++ src/components/settings/UserDetails.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export const UserDetails = () => {
-  return (
-    <div>UserDetails</div>
-  )
-}
+  return <div>UserDetails</div>;
+};
