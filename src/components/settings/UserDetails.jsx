c6510415f97daedc09ec8dce621058a17d4f5aeb
Kuba <czubakjakub94@gmail.com>
2023-03-24 14:31:47 +0100

    edited settings section

--- src/components/settings/UserDetails.jsx
+++ src/components/settings/UserDetails.jsx
@@ -5,15 +5,32 @@ import SaveIcon from '@mui/icons-material/Save';
 import { useState } from 'react';
 
 export const UserDetails = () => {
+  const [user, setUser] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
   return (
     <div className={styles.userDetails_container}>
       <div className={styles.user_inputs}>
-        <p className={styles.userDetails_title}>Profile details</p>
-        <TextField label="Imię" variant="outlined" value="asdad"  />
-        <TextField label="Nazwisko" variant="outlined" />
-        <TextField label="Email" variant="outlined" />
-        <TextField label="Hasło" variant="outlined" type="password" />
-        <TextField label="Powtórz hasło" variant="outlined" type="password" />
+        <p className={styles.userDetails_title}>Profile details:</p>
+        <TextField label="Imię" variant="outlined" className={styles.wider_textfield} />
+        <TextField label="Nazwisko" variant="outlined" className={styles.wider_textfield} />
+        <TextField label="Email" variant="outlined" className={styles.wider_textfield} />
+        <TextField
+          label="Hasło"
+          variant="outlined"
+          type="password"
+          className={styles.wider_textfield}
+        />
+        <TextField
+          label="Powtórz hasło"
+          variant="outlined"
+          type="password"
+          className={styles.wider_textfield}
+        />
         <Button variant="contained" endIcon={<SaveIcon />}>
           Zapisz
         </Button>
