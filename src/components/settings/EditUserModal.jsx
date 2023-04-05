0327577516a7e074853dfe17a36c46ef48974e69
Kuba <czubakjakub94@gmail.com>
2023-04-05 21:15:18 +0200

    edited contact section

---
+++ src/components/settings/EditUserModal.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './css/EditUserModal.module.css';
+
+export const EditUserModal = () => {
+  return <div className={styles.modal_container}>
+    
+  </div>;
+};
