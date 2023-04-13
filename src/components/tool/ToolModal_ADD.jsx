10e2f0f910e7916cc0cabe0e7f51217b39c98a8d
Kuba <czubakjakub94@gmail.com>
2023-04-13 14:09:10 +0200

    edited overall apperance

--- src/components/tool/ToolModal_ADD.jsx
+++ src/components/tool/ToolModal_ADD.jsx
@@ -45,7 +45,7 @@ export const ToolModal_ADD = ({ open, onClose, item }) => {
   return ReactDom.createPortal(
     <div className={styles.modal_container}>
       <div className={styles.modal}>
-        <img src={require('../../assets/tool_diameter.jpg')} alt="Tool diameter" />
+        <img src={require('../../assets/tool_diameter.png')} alt="Tool diameter" />
         <div className={styles.modal_header}>
           <h2>Create tool</h2>
         </div>
