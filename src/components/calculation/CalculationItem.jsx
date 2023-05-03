012ebfe6fafd66f54e5ceac13f51193de0509dc8
Kuba <czubakjakub94@gmail.com>
2023-05-03 20:25:31 +0200

    added pie chart

--- src/components/calculation/CalculationItem.jsx
+++ src/components/calculation/CalculationItem.jsx
@@ -3,6 +3,18 @@ import styles from './css/CalculationItem.module.css';
 import { Typography, Breadcrumbs, TextField, Select, MenuItem, InputLabel } from '@mui/material';
 import { useState } from 'react';
 import { DatePicker } from '@mui/x-date-pickers';
+import { Chart } from 'react-google-charts';
+
+export const data = [
+  ['Cost name', 'PLN'],
+  ['Work', 11],
+  ['Eat', 2],
+  ['Commute', 2]
+];
+
+export const options = {
+  title: 'Cost diagram'
+};
 
 export const CalculationItem = () => {
   const [status, setStatus] = useState('Pending');
@@ -35,8 +47,14 @@ export const CalculationItem = () => {
           </Select>
         </div>
         <div className={styles.line} />
+        <div className={styles.cost_header}>
+          <Typography variant="h6" component="div">
+            Department maintenance cost
+          </Typography>
+        </div>
+
         <div className={styles.calculation_cost}>
-          <TextField label="Monthly cost" variant="filled" disabled />
+          <TextField label="Maintenance cost" variant="filled" disabled />
           <div>=</div>
           <TextField label="Employee costs" variant="outlined" />
           <div>+</div>
@@ -50,6 +68,16 @@ export const CalculationItem = () => {
           <div>+</div>
           <TextField label="Depreciation" variant="outlined" />
         </div>
+        <div className={styles.pie_chart}>
+          <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={'100%'}
+            height={'400px'}
+          />
+        </div>
+        <div className={styles.line} />
       </div>
     </>
   );
