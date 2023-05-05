3ad55aa088e1922ab9bcfc40a5c5ad16414333bf
Kuba <czubakjakub94@gmail.com>
2023-05-05 10:12:16 +0200

    edited calculation component

--- src/components/calculation/CalculationItem.jsx
+++ src/components/calculation/CalculationItem.jsx
@@ -8,11 +8,13 @@ import {
   MenuItem,
   InputLabel,
   InputAdornment,
-  Tooltip
+  Tooltip,
+  IconButton
 } from '@mui/material';
 import { useState } from 'react';
 import { DatePicker } from '@mui/x-date-pickers';
 import { Chart } from 'react-google-charts';
+import DownloadingIcon from '@mui/icons-material/Downloading';
 
 export const options = {
   title: 'Cost diagram'
@@ -57,179 +59,335 @@ export const CalculationItem = () => {
           </Select>
         </div>
         <div className={styles.line} />
-        <div className={styles.cost_header}>
-          <Typography variant="h6" component="div">
-            Department maintenance cost
-          </Typography>
-        </div>
+        <div className={styles.department_maintenance_cost}>
+          <div className={styles.cost_header}>
+            <Typography variant="h6" component="div">
+              Department maintenance cost
+            </Typography>
+          </div>
 
-        <div className={styles.calculation_cost}>
-          <Tooltip title="Employee costs">
-            <TextField
-              label="Employee costs"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>+</div>
-          <Tooltip title="Machine power consumption">
-            <TextField
-              label="Power consumption"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">kW</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>x</div>
-          <Tooltip title="Machine operating hours">
-            <TextField
-              label="Operating hours"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">h</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>x</div>
-          <Tooltip title="Price PLN/kWh">
-            <TextField
-              label="Price PLN/kWh"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN/kWh</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>+</div>
-          <Tooltip title="Media price">
-            <TextField
-              label="Media"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>+</div>
-          <Tooltip title="Depreciation price">
-            <TextField
-              label="Depreciation"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>+</div>
-          <Tooltip title="Tools price">
-            <TextField
-              label="Tools"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>+</div>
-          <Tooltip title="Service price">
-            <TextField
-              label="Service"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>+</div>
-          <Tooltip title="Leasing/Installment price">
-            <TextField
-              label="Leasing/Installment"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>+</div>
-          <Tooltip title="Variable costs I price">
-            <TextField
-              label="Variable costs I"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>+</div>
-          <Tooltip title="Variable costs II price">
-            <TextField
-              label="Variable costs II"
-              variant="outlined"
-              size="small"
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN</InputAdornment>
-              }}
-            />
-          </Tooltip>
-          <div>=</div>
-          <Tooltip title="Department maintenance cost">
-            <TextField
-              label="Maintenance cost"
-              variant="filled"
-              disabled
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN (net)</InputAdornment>
-              }}
-            />
-          </Tooltip>
+          <div className={styles.calculation_cost}>
+            <Tooltip title="Employee costs">
+              <TextField
+                label="Employee costs"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Machine power consumption">
+              <TextField
+                label="Power consumption"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">kW</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>x</div>
+            <Tooltip title="Machine operating hours">
+              <TextField
+                label="Operating hours"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">h</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>x</div>
+            <Tooltip title="Price PLN/kWh">
+              <TextField
+                label="Price PLN/kWh"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN/kWh</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Media price">
+              <TextField
+                label="Media"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Depreciation price">
+              <TextField
+                label="Depreciation"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Tools price">
+              <TextField
+                label="Tools"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Service price">
+              <TextField
+                label="Service"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Leasing/Installment price">
+              <TextField
+                label="Leasing/Installment"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Variable costs I price">
+              <TextField
+                label="Variable costs I"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Variable costs II price">
+              <TextField
+                label="Variable costs II"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>=</div>
+            <Tooltip title="Department maintenance cost">
+              <TextField
+                label="Maintenance cost"
+                variant="filled"
+                disabled
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN (net)</InputAdornment>
+                }}
+              />
+            </Tooltip>
 
-          <Tooltip title="Hourly department maintenance cost">
-            <TextField
-              label="Maintenance cost"
-              variant="filled"
-              disabled
-              sx={{ width: '280px' }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">PLN (net)/h</InputAdornment>
-              }}
+            <Tooltip title="Hourly department maintenance cost">
+              <TextField
+                label="Maintenance cost"
+                variant="filled"
+                disabled
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN (net)/h</InputAdornment>
+                }}
+              />
+            </Tooltip>
+          </div>
+          <div className={styles.pie_chart}>
+            <Chart
+              chartType="PieChart"
+              data={data}
+              options={options}
+              width={'100%'}
+              height={'400px'}
             />
-          </Tooltip>
+          </div>
+          <div className={styles.line} />
         </div>
-        <div className={styles.pie_chart}>
-          <Chart
-            chartType="PieChart"
-            data={data}
-            options={options}
-            width={'100%'}
-            height={'400px'}
-          />
+        <div className={styles.cnc_order_valuation}>
+          <div className={styles.cost_header}>
+            <Typography variant="h6" component="div">
+              CNC order valuation
+            </Typography>
+          </div>
+          <div className={styles.cnc_calculation_cost}>
+            <Tooltip title="Machine time in CAM simulation">
+              <TextField
+                label="CAM Time"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">h</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>x</div>
+            <Tooltip title="Factor (Mounting, machine arming)">
+              <TextField label="Factor" variant="outlined" size="small" sx={{ width: '280px' }} />
+            </Tooltip>
+            <div>=</div>
+            <Tooltip title="Machine working time">
+              <TextField
+                label="CNC Time"
+                size="small"
+                variant="filled"
+                disabled
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">h</InputAdornment>
+                }}
+              />
+            </Tooltip>
+          </div>
+          <div className={styles.input}>
+            <Tooltip title="Hourly rate">
+              <TextField
+                label="Hourly rate"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN/h</InputAdornment>
+                }}
+              />
+            </Tooltip>
+          </div>
+          <div className={styles.input}>
+            <Tooltip title="Material cost">
+              <TextField
+                label="Material cost"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <Tooltip title="Download material cost from BOX">
+              <IconButton>
+                {' '}
+                <DownloadingIcon />{' '}
+              </IconButton>
+            </Tooltip>
+            <p className={styles.box_info}>Material list included in the box</p>
+          </div>
+          <div className={styles.input}>
+            <Tooltip title="Tool cost">
+              <TextField
+                label="Tool cost"
+                variant="outlined"
+                size="small"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <Tooltip title="Download tool cost from BOX">
+              <IconButton>
+                {' '}
+                <DownloadingIcon />{' '}
+              </IconButton>
+            </Tooltip>
+            <p className={styles.box_info}>Tool list included in the box</p>
+          </div>
+          <div className={styles.cnc_calculation_cost}>
+            <Tooltip title="Machine working time">
+              <TextField
+                label="CNC Time"
+                size="small"
+                variant="outlined"
+                disabled
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">h</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>x</div>
+            <Tooltip title="Hourly rate">
+              <TextField
+                label="Hourly rate"
+                size="small"
+                variant="outlined"
+                disabled
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN/h</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Material cost">
+              <TextField
+                label="Material cost"
+                size="small"
+                variant="outlined"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>+</div>
+            <Tooltip title="Tool cost">
+              <TextField
+                label="Tool cost"
+                size="small"
+                variant="outlined"
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+            <div>=</div>
+            <Tooltip title="CNC order valuation">
+              <TextField
+                label="Valuation"
+                size="small"
+                variant="filled"
+                disabled
+                sx={{ width: '280px' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">PLN</InputAdornment>
+                }}
+              />
+            </Tooltip>
+          </div>
         </div>
-        <div className={styles.line} />
       </div>
     </>
   );
