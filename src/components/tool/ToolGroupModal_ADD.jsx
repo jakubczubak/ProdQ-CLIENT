0d8bfce8f4ac52bd06c5c1dd349a136ea325a8d7
Kuba <czubakjakub94@gmail.com>
2023-06-02 09:49:42 +0200

    added tool images

--- src/components/tool/ToolGroupModal_ADD.jsx
+++ src/components/tool/ToolGroupModal_ADD.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import styles from './css/ToolModal.module.css';
-import { Stack, Button } from '@mui/material';
+import { Stack, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
 import { toolGroupValidationSchema } from './validationSchema/toolGroupValidationSchema';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -61,6 +61,46 @@ export const ToolGroupModal_ADD = ({ open, onClose }) => {
                     onChange={onChange}
                     label="Tool group name"
                   />
+                )}
+              />
+              <Controller
+                name="toolGroupType"
+                control={control}
+                render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => (
+                  <FormControl fullWidth error={error}>
+                    <InputLabel id="select-label">Type of tool group</InputLabel>
+                    <Select
+                      labelId="select-label"
+                      value={value}
+                      onBlur={onBlur}
+                      label="Type of tool group"
+                      onChange={onChange}>
+                      <MenuItem value={1}>End mill</MenuItem>
+                      <MenuItem value={2}>Radius end mill</MenuItem>
+                      <MenuItem value={3}>Ball end mill</MenuItem>
+                      <MenuItem value={4}>Milling cutter head</MenuItem>
+                      <MenuItem value={5}>Dovetail end mill</MenuItem>
+                      <MenuItem value={6}>Tapered end mill</MenuItem>
+                      <MenuItem value={7}>Rounded tapered end mill</MenuItem>
+                      <MenuItem value={8}>T-slot milling cutter</MenuItem>
+                      <MenuItem value={9}>Ball cutter</MenuItem>
+                      <MenuItem value={10}>Engraving cutter</MenuItem>
+                      <MenuItem value={11}>Counterbore drill</MenuItem>
+                      <MenuItem value={12}>Countersink</MenuItem>
+                      <MenuItem value={13}>Drill</MenuItem>
+                      <MenuItem value={14}>Chamfer drill</MenuItem>
+                      <MenuItem value={15}>Tap</MenuItem>
+                      <MenuItem value={16}>Reamer</MenuItem>
+                      <MenuItem value={17}>Boring bar</MenuItem>
+                      <MenuItem value={18}>Thread milling cutter</MenuItem>
+                      <MenuItem value={19}>Tapered thread milling cutter</MenuItem>
+                      <MenuItem value={21}>Saw</MenuItem>
+                      <MenuItem value={22}>Milling cutters</MenuItem>
+                      <MenuItem value={23}>Turning inserts</MenuItem>
+                      <MenuItem value={24}>Turning tools</MenuItem>
+                      <MenuItem value={25}>Others</MenuItem>
+                    </Select>
+                  </FormControl>
                 )}
               />
 
