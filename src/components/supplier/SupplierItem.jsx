b04b5d84d5c377da49c7409d72dbedb72e792c3d
Kuba <czubakjakub94@gmail.com>
2023-07-22 15:14:53 +0200

    edited supplier component

--- src/components/supplier/SupplierItem.jsx
+++ src/components/supplier/SupplierItem.jsx
@@ -29,21 +29,24 @@ export const SupplierItem = ({ item }) => {
   return (
     <>
       <div className={styles.supplierItem_info_container}>
-        <Tooltip title="Edit" placement="top">
-          <div className={styles.editIcon}>
-            <EditIcon
-              fontSize={'5px'}
-              onClick={() => {
-                navigate(`/supplier/edit`, { state: { item } });
-              }}
-            />
-          </div>
-        </Tooltip>
-        <Tooltip title="Delete" placement="top">
-          <div className={styles.deleteIcon}>
-            <DeleteIcon fontSize={'5px'} onClick={() => setOpenDeleteModal(true)} />
-          </div>
-        </Tooltip>
+        <div className={styles.action_wrapper}>
+          <Tooltip title="Edit" placement="top">
+            <div className={styles.editIcon}>
+              <EditIcon
+                fontSize={'5px'}
+                onClick={() => {
+                  navigate(`/supplier/edit`, { state: { item } });
+                }}
+              />
+            </div>
+          </Tooltip>
+          <Tooltip title="Delete" placement="top">
+            <div className={styles.deleteIcon}>
+              <DeleteIcon fontSize={'5px'} onClick={() => setOpenDeleteModal(true)} />
+            </div>
+          </Tooltip>
+        </div>
+
         <div className={styles.supplierItem_info}>
           <div className={styles.supplierItem_info_logo}>
             <img src={item.companyLogo ? item.companyLogo : noImage} alt="" />
@@ -53,6 +56,18 @@ export const SupplierItem = ({ item }) => {
           <div className={styles.btn_wrapper}>
             <Button variant="contained">View page</Button>
             <Button variant="outlined">Send message</Button>
+          </div>
+          <div className={styles.tagList_wrapper}>
+            <div className={styles.tagList_header}>
+              <p>TAGS</p>
+            </div>
+            <div className={styles.tagList}>
+              {item.tagList.map((tag, index) => (
+                <p key={index} className={styles.tag_item}>
+                  {tag}
+                </p>
+              ))}
+            </div>
           </div>
         </div>
       </div>
