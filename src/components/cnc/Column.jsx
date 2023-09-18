f137a8fe604dfc12f2e2baffb96b2e59413577d9
Kuba <czubakjakub94@gmail.com>
2023-09-18 14:52:27 +0200

    edited dnd

--- src/components/cnc/Column.jsx
+++ src/components/cnc/Column.jsx
@@ -3,13 +3,30 @@ import styles from './css/Column.module.css';
 import { Task } from './Task';
 import { StrictModeDroppable as Droppable } from '../helpers/StrictModeDroppable';
 import image from '../../assets/BACAR1000.png';
+import CloudSyncOutlinedIcon from '@mui/icons-material/CloudSyncOutlined';
+import { Tooltip } from '@mui/material';
 
 export const Column = ({ id, title, tasks }) => {
   return (
     <div key={id} className={styles.column}>
       <h3 className={styles.column_header}>{title}</h3>
       {(id === 'column-2' || id === 'column-3') && (
-        <img src={image} alt="" className={styles.image} />
+        <>
+          {' '}
+          <Tooltip title="Sync with machine" placement="top">
+            <CloudSyncOutlinedIcon
+              color="action"
+              sx={{
+                position: 'absolute',
+                top: '10px',
+                right: '50%',
+                transform: 'translateX(50%)',
+                cursor: 'pointer'
+              }}
+            />
+          </Tooltip>
+          <img src={image} alt="" className={styles.image} />
+        </>
       )}
       <Droppable droppableId={id}>
         {(provided) => (
