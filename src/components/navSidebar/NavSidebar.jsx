4fd0028870f81e0aa5b565de899511fa08362cdb
Kuba <czubakjakub94@gmail.com>
2023-11-28 17:34:11 +0100

    added wallpaper

--- src/components/navSidebar/NavSidebar.jsx
+++ src/components/navSidebar/NavSidebar.jsx
@@ -105,8 +105,7 @@ export const NavSidebar = () => {
             <li
               onClick={() => {
                 setOpenLogoutModal(true);
-              }}
-            >
+              }}>
               <LogoutIcon style={{ color: 'white' }} />
               <button>Logout</button>
             </li>
