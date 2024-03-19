043de6a7d2d9f63d138678ed50f81dcda25eaa1b
Kuba <czubakjakub94@gmail.com>
2024-03-19 15:02:11 +0100

    edited project stuff

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,6 @@
 // Importy zewnętrzne
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
 
 // Importy lokalne
 import styles from './css/Main.module.css';
@@ -14,9 +13,6 @@ import { ToolGroupItemDetails } from '../tool/ToolGroupItemDetails';
 import { RecycleItem } from '../recycling/RecycleItem';
 import { SupplierList } from '../supplier/SupplierList';
 import { SupplierForm } from '../supplier/SupplierForm';
-import { departmentCostManager } from '../settings/service/departmentCostManager';
-import { Loader } from '../common/Loader';
-import { Error } from '../common/Error';
 import { OrderList } from '../order/OrderList';
 import { OrderItem } from '../order/OrderItem';
 import { Dashboard } from '../dashboard/Dashboard';
@@ -25,43 +21,31 @@ import { ProjectListItem } from '../projects/ProjectListItem';
 import { NotFound } from '../common/NotFound';
 
 export const Main = () => {
-  const { data, isLoading, isError } = useQuery(
-    ['defaultValues'],
-    departmentCostManager.getDefaultDepartmentCost
-  ); // fetch default department cost
-
-  if (isLoading) return <Loader />;
-  if (isError)
-    return (
-      <Error message="Error fetching default department cost values! Please try again later!" />
-    );
-
-  if (!isError && !isLoading && data)
-    return (
-      <div className={styles.main_container}>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/dashboard" />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/materials" element={<MaterialGroupList />} />
-          <Route path="/materials/new" element={<MaterialGroupList open={true} />} />
-          <Route path="/materials/:id" element={<MaterialGroupItemDetails />} />
-          <Route path="/tools" element={<ToolGroupList />} />
-          <Route path="/tools/new" element={<ToolGroupList open={true} />} />
-          <Route path="/tools/:id" element={<ToolGroupItemDetails />} />
-          <Route path="/orders" element={<OrderList />} />
-          <Route path="/order/new" element={<OrderItem />} />
-          <Route path="/order/edit" element={<OrderItem />} />
-          <Route path="/recycling" element={<RecycleList />} />
-          <Route path="/recycling/wtc" element={<RecycleItem />} />
-          <Route path="/suppliers" element={<SupplierList />} />
-          <Route path="/supplier/new" element={<SupplierForm />} />
-          <Route path="/supplier/edit" element={<SupplierForm />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/settings/department" element={<Settings tab={'4'} />} />
-          <Route path="/projects" element={<ProjectList />} />
-          <Route path="/projects/:id" element={<ProjectListItem />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </div>
-    );
+  return (
+    <div className={styles.main_container}>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/materials" element={<MaterialGroupList />} />
+        <Route path="/materials/new" element={<MaterialGroupList open={true} />} />
+        <Route path="/materials/:id" element={<MaterialGroupItemDetails />} />
+        <Route path="/tools" element={<ToolGroupList />} />
+        <Route path="/tools/new" element={<ToolGroupList open={true} />} />
+        <Route path="/tools/:id" element={<ToolGroupItemDetails />} />
+        <Route path="/orders" element={<OrderList />} />
+        <Route path="/order/new" element={<OrderItem />} />
+        <Route path="/order/edit" element={<OrderItem />} />
+        <Route path="/recycling" element={<RecycleList />} />
+        <Route path="/recycling/wtc" element={<RecycleItem />} />
+        <Route path="/suppliers" element={<SupplierList />} />
+        <Route path="/supplier/new" element={<SupplierForm />} />
+        <Route path="/supplier/edit" element={<SupplierForm />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/settings/department" element={<Settings tab={'4'} />} />
+        <Route path="/projects" element={<ProjectList />} />
+        <Route path="/projects/:id" element={<ProjectListItem />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 };
