91c33a4166b75f15c4d1db67bf946af786652431
Kuba <czubakjakub94@gmail.com>
2024-03-25 09:23:05 +0100

    edited notification section

--- src/components/common/Notifications.jsx
+++ src/components/common/Notifications.jsx
@@ -1,7 +1,13 @@
 //Importy zewnętrzne
 import { Snackbar } from '@mui/material';
 import ReactDom from 'react-dom';
+import Lottie from 'lottie-react';
+//Importy wewnętrzne
 import styles from './css/Notifications.module.css';
+import info from '../../assets/Lottie/alert/info.json';
+import success from '../../assets/Lottie/alert/success.json';
+import error from '../../assets/Lottie/alert/error.json';
+import warning from '../../assets/Lottie/alert/warning.json';
 
 export const Notifications = ({ open, onClose, severity, message, anchorOrigin }) => {
   //dopisac kod do seveity
@@ -13,9 +19,18 @@ export const Notifications = ({ open, onClose, severity, message, anchorOrigin }
       onClose={onClose}
       anchorOrigin={anchorOrigin ? anchorOrigin : { vertical: 'bottom', horizontal: 'center' }}>
       <button className={styles.button}>
-        <svg className={styles.bell} viewBox="0 0 448 512">
-          <path d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"></path>
-        </svg>
+        {severity === 'info' && (
+          <Lottie animationData={info} className={styles.animation} loop={false} />
+        )}
+        {severity === 'success' && (
+          <Lottie animationData={success} className={styles.animation} loop={false} />
+        )}
+        {severity === 'error' && (
+          <Lottie animationData={error} className={styles.animation} loop={false} />
+        )}
+        {severity === 'warning' && (
+          <Lottie animationData={warning} className={styles.animation} loop={false} />
+        )}
         {message}
       </button>
     </Snackbar>,
