9d9236cb8113d4ae93c75c3760fa06cd5c9025e5
Kuba <czubakjakub94@gmail.com>
2024-04-16 13:35:01 +0200

    edited HideButton component

--- src/components/main/HideButton.jsx
+++ src/components/main/HideButton.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './css/HideButton.module.css';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import { toggleSidebar } from '../../redux/actions/Action';
+import { Tooltip } from '@mui/material';
 
 export const HideButton = () => {
   const isNavbarHidden = useSelector((state) => state.sidebar);
@@ -14,12 +18,12 @@ export const HideButton = () => {
   };
 
   return (
-    <div className={`${styles.container} ${isNavbarHidden && styles.navHidden}`}>
-      {isNavbarHidden ? (
-        <ArrowRightIcon color="action" onClick={handleClick} />
-      ) : (
-        <ArrowLeftIcon color="action" onClick={handleClick} />
-      )}
-    </div>
+    <Tooltip title={isNavbarHidden ? 'Show Sidebar' : 'Hide Sidebar'} arrow placement="right">
+      <div
+        className={`${styles.container} ${isNavbarHidden && styles.navHidden}`}
+        onClick={handleClick}>
+        {isNavbarHidden ? <ArrowRightIcon color="action" /> : <ArrowLeftIcon color="action" />}
+      </div>
+    </Tooltip>
   );
 };
