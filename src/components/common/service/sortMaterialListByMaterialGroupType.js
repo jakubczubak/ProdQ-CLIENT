80bffc44f3fa1f5e0932123c59cf8c29952ebbc2
Jackob <czubakjakub94@gmail.com>
2025-01-22 10:39:52 +0100

    edited MaterialGroupItem

--- src/components/common/service/sortMaterialListByMaterialGroupType.js
+++ src/components/common/service/sortMaterialListByMaterialGroupType.js
@@ -1,6 +1,13 @@
 export const sortMaterialListByMaterialGroupType = (materialList, materialGroupType) => {
   if (materialGroupType === 'Plate') {
-    return materialList.sort((a, b) => a.z - b.z);
+    return materialList.sort((a, b) => {
+      // Najpierw sortowanie po 'z'
+      if (a.z !== b.z) return a.z - b.z;
+      // Jeśli 'z' są równe, sortowanie po 'x'
+      if (a.x !== b.x) return a.x - b.x;
+      // Jeśli 'z' i 'x' są równe, sortowanie po 'y'
+      return a.y - b.y;
+    });
   } else {
     return materialList.sort((a, b) => a.diameter - b.diameter);
   }
