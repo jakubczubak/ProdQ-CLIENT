b693882c5bd53098a548c00a39bd6871f758e598
Jackob <czubakjakub94@gmail.com>
2025-02-26 08:59:32 +0100

    edited NCProgramList.jsx

--- src/components/productionQueue/NCProgramsList.jsx
+++ src/components/productionQueue/NCProgramsList.jsx
@@ -5,7 +5,6 @@ import styles from './css/productionQueue.module.css';
 
 export const NCProgramsList = ({ programs, droppableId, title }) => {
   // Sortowanie programów po dacie deadline
-  const sortedPrograms = [...programs].sort((a, b) => new Date(a.deadline) - new Date(b.deadline));
 
   return (
     <div className={styles.nc_programs_container}>
@@ -17,10 +16,10 @@ export const NCProgramsList = ({ programs, droppableId, title }) => {
             ref={provided.innerRef}
             {...provided.droppableProps}
             style={{ minHeight: '240px' }}>
-            {sortedPrograms.length === 0 && !snapshot.isDragging && !snapshot.isDraggingOver && (
+            {programs.length === 0 && !snapshot.isDragging && !snapshot.isDraggingOver && (
               <div className={styles.placeholder}>Drop program here!</div>
             )}
-            {sortedPrograms.map((program, index) => (
+            {programs.map((program, index) => (
               <NCProgram program={program} key={program.id} index={index} />
             ))}
             {provided.placeholder}
